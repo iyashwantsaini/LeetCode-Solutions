bd2affeccd2d67fbe07cbb00b7295bb54d45999b
meyash <yashsn2127@gmail.com>
2021-01-05 23:53:17 +0530

    day-7

--- 23.merge-k-sorted-lists.cpp
+++ 23.merge-k-sorted-lists.cpp
@@ -78,37 +78,46 @@
 using namespace std;
 typedef long long ll;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n=lists.size();
         if(n==0){
-            ListNode *l;
-            return l;
+            return NULL;
         }
         ListNode *l=new ListNode();
         ListNode *head=l;
-        int flag=0;
-
-        for(int i=0;i<n;i++){
-            
+        ListNode *prev;
+        int _empty=1;
+        while(1){
+            int val=INT_MAX;
+            int index=0;
+            int possible=0;
+            for(int i=0;i<n;i++){
+                if(lists[i]!=NULL&&lists[i]->val<val){
+                    val=lists[i]->val;
+                    index=i;
+                }
+            }
+            // now we have minimum in current
+            if(val==INT_MAX){
+                break;
+            }else{
+                // create new node
+                _empty=0;
+                l->val=val;
+                prev=l;
+                l->next=new ListNode();
+                l=l->next;
+                lists[index]=lists[index]->next;
+            }
         }
-
-        if(flag==0){
-            ListNode *l;
-            return l;
-        }else{
-            return head;
+        if(head->next!=NULL){
+            prev->next=NULL;
         }
+        if(_empty){
+            head=NULL;
+        }
+        return head;
     }
 };
-// @lc code=end
-
